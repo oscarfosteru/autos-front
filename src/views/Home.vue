21dac0c76c9eef9674fdc0472c6b1a7cca70a89e
OscarBarriosIS <oscar.barrios@imagen-social.com>
2024-11-27 09:59:55 -0600

    front

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Hola desde jenkins</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
